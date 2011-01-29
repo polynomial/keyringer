2c26d9e2130bb5751205963cb8ff8ca459226f0f
Silvio Rhatto <rhatto@riseup.net>
2011-01-29 18:50:09 -0200

    Moving parser to keyringer module; actions should be in a submodule

--- lib/keyringer.rb
+++ lib/keyringer.rb
@@ -1,9 +1,10 @@
 # internal requires
 $:.unshift File.dirname(__FILE__)
 require 'keyring'
-require 'keyringer/decrypt'
 require 'keyringer/bash_wrapper'
+require 'keyringer/parser'
+require 'keyringer/actions/decrypt'
 
-module Keyring
+module Keyringer
   VERSION = '2.0-alpha'
 end
