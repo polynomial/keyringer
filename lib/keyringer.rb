5196fb316c1afc2b6b0d11132e105c7f1fb841fe
Zapata <zapata@xs4all.nl>
2011-01-29 22:14:19 +0100

    preliminary support for recipient management 2

--- lib/keyringer.rb
+++ lib/keyringer.rb
@@ -3,6 +3,7 @@ $:.unshift File.dirname(__FILE__)
 require 'keyring'
 require 'keyringer/decrypt'
 require 'keyringer/bash_wrapper'
+require 'keyringer/recipients'
 
 module Keyring
   VERSION = '2.0-alpha'
